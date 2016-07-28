c5240ba2fff1db0c8485b1b74e75b9cd5f3ede93
brett <brett@debian-laptop>
2016-07-28 12:45:41 -0700

    Removed comments in eleanor/utils

--- eleanor/utils.py
+++ eleanor/utils.py
@@ -8,12 +8,6 @@ def get_logger(module_name):
     """Get a logger with created with values from settings/logging.conf and
     using time.gmtime
     """
-#    here = os.path.abspath(os.path.dirname(__file__))
-#    logging_conf_path = '{0}/{1}'.format(here, 'settings/logging.conf')
-#    logging.config.fileConfig(logging_conf_path)
-#    logging.Formatter.converter = time.gmtime
-#    logger = logging.getLogger(module_name)
-
     logger = logging.getLogger(module_name)
     logger.setLevel(logging.DEBUG)
     handler = logging.handlers.TimedRotatingFileHandler(
