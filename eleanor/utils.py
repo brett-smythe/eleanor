450a3a7aa0aa3a599b6e09d29c89d5d6939b1742
brett <brett@debian-laptop>
2016-10-23 00:39:26 -0700

    Changed logging config to check for prod environment variable

--- eleanor/utils.py
+++ eleanor/utils.py
@@ -1,25 +1,26 @@
 """General eleanor utilities"""
+import os
 import logging
 import logging.config
 import time
-
-dev = False
 
 
 def get_logger(module_name):
     """Get a logger with created with values from settings/logging.conf and
     using time.gmtime
     """
+
     logger = logging.getLogger(module_name)
     logger.setLevel(logging.DEBUG)
-    if dev:
-        handler = logging.handlers.TimedRotatingFileHandler(
-            '/tmp/eleanor.log', 'midnight', 1, 0, 'utf-8', False,
-            True
-        )
+    if 'RUN_ENV' in os.environ:
+        if os.environ['RUN_ENV'] == 'production':
+            handler = logging.handlers.TimedRotatingFileHandler(
+                '/var/log/eleanor/eleanor.log', 'midnight', 1, 0, 'utf-8',
+                False, True
+            )
     else:
         handler = logging.handlers.TimedRotatingFileHandler(
-            '/var/log/eleanor/eleanor.log', 'midnight', 1, 0, 'utf-8', False,
+            '/tmp/eleanor.log', 'midnight', 1, 0, 'utf-8', False,
             True
         )
     formatter = logging.Formatter(
