57f7a8605f101e4699070476f3335bcdcbf053ff
brett <brett@debian-laptop>
2016-11-02 01:41:26 -0700

    Reverted model_setup changes

--- eleanor/models/model_setup.py
+++ eleanor/models/model_setup.py
@@ -4,9 +4,9 @@ from eleanor.clients.postgres import client
 
 from eleanor.utils import get_logger
 
-with client.GetDBSession as db_session:
+engine = client.get_db_engine()
 
-    logger = get_logger(__name__)
-    logger.info('Setting up database for eleanor')
+logger = get_logger(__name__)
+logger.info('Setting up database for eleanor')
 
-    Base.metadata.create_all()
+Base.metadata.create_all(engine)
